fd95452124f7ab4f1d9f60ff9f18d35223cd1870
wisnhu wardhana <warddhana@gmail.com>
2019-08-20 16:16:54 +0700

    change memcached

--- application/config/memcached.php
+++ application/config/memcached.php
@@ -10,7 +10,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	See: https://codeigniter.com/user_guide/libraries/caching.html#memcached
 |
 */
-<<<<<<< HEAD
+
 $config = array(
 	'default' => array(
 		'hostname' => 'cachebsm.hqfv4d.cfg.use1.cache.amazonaws.com',
