1ed6c11226853301d0f81dee0c9995bd62fe9609
wisnhu wardhana <warddhana@gmail.com>
2019-08-20 12:46:57 +0700

    test

--- application/config/memcached.php
+++ application/config/memcached.php
@@ -12,7 +12,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $config = array(
 	'default' => array(
-		'hostname' => 'bsm.jtw0ue.cfg.use1.cache.amazonaws.com',
+		'hostname' => 'bsmcoba.jtw0ue.cfg.use1.cache.amazonaws.com',
 		'port'     => '11211',
 		'weight'   => '1',
 	),
