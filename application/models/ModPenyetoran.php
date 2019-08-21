7f1beeb2ae04e30712c09f20248bda70cb1a8380
wisnhu wardhana <warddhana@gmail.com>
2019-08-21 15:22:28 +0700

    change name table

--- application/models/ModPenyetoran.php
+++ application/models/ModPenyetoran.php
@@ -19,7 +19,7 @@ class modPenyetoran extends CI_Model{
     }
     
     public function InsertDetail($data){
-        $this->tabel = 'detailPenyetoran';
+        $this->tabel = 'detailpenyetoran';
         $this->query = $this->db->insert_batch($this->tabel, $data);
         return $this->query; 
     }
@@ -31,7 +31,7 @@ class modPenyetoran extends CI_Model{
     }
 
     public function DeleteDetail($noNota){
-        $this->tabel = 'detailPenyetoran';
+        $this->tabel = 'detailpenyetoran';
         $this->query = $this->db->delete($this->tabel, $noNota); 
         return $this->query;
     }
@@ -43,7 +43,7 @@ class modPenyetoran extends CI_Model{
 	}
 
     public function GetWhereDetail($data){ 
-        $this->tabel = 'detailPenyetoran';
+        $this->tabel = 'detailpenyetoran';
         $this->query=$this->db->get_where($this->tabel, $data);
         return $this->query->result_array();
     }
