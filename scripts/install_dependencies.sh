0f6d13244f1f5497aeaae5f4e792c893ae9f74be
Rezky <rezkydermawan97@gmail.com>
2019-08-24 03:23:13 +0700

    Fix sed script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,5 +4,5 @@ amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 yum install -y httpd
 yum install -y php-pecl-memcached
 sed 's/^\(session.save_handler\).*/\1 = memcached/' -i /etc/php.ini
-sed '\#^\(session.save_handler\).*#a\session.save_path = "cachebsm.hqfv4d.cfg.use1.cache.amazonaws.com:11211?persistent=1&weight=1&timeout=1&retry_interval=15"' -i /etc/php.ini
+sed '\#^\(session.save_handler\).*#{n;s#.*#session.save_path = "cachebsm.hqfv4d.cfg.use1.cache.amazonaws.com:11211"#}' -i /etc/php.ini
 service httpd restart
