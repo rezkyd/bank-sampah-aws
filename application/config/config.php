b42f4159991fd197b30a1dbb4403b64d3f959850
Rezky <rezkydermawan97@gmail.com>
2019-08-25 22:20:45 +0700

    change base url to https

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://banksampah.ddns.net/';
+$config['base_url'] = 'https://banksampah.ddns.net/';
 
 /*
 |--------------------------------------------------------------------------
