fdacc3075cdce22d9b6f01832ca6c5e7663895e9
Rezky <rezkydermawan97@gmail.com>
2019-08-25 18:59:24 +0700

    testing dns 2

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://banksampah.ddns.net/';
+$config['base_url'] = 'http://bsm-web-loadbalancer-632989805.us-east-1.elb.amazonaws.com/';
 
 /*
 |--------------------------------------------------------------------------
