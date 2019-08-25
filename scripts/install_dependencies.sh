b273755a60ef11735377394a205f9dd7f920da0e
Rezky <rezkydermawan97@gmail.com>
2019-08-25 18:23:17 +0700

    testing elb with https

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,5 +5,9 @@ yum install -y httpd
 yum install -y php-pecl-memcached
 sed 's/^\(session.save_handler\).*/\1 = memcached/' -i /etc/php.ini
 sed '\#^\(session.save_handler\).*#{n;s#.*#session.save_path = "cachebsm.hqfv4d.cfg.use1.cache.amazonaws.com:11211"#}' -i /etc/php.ini
-
+yum install -y mod_ssl
+wget -r --no-parent -A 'epel-release-*.rpm' http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/
+rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.rpm
+yum-config-manager --enable epel*
+yum install -y certbot python2-certbot-apache
 
